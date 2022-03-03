7db6d7e7e7d9b22bdd0eb3f5daae7851da1b463a
Ray <89287773+RayNanta@users.noreply.github.com>
2022-03-03 13:28:51 +0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: [
+    "./src/app.js",
+    "./src/components/Navbar.js",
+    "./src/components/Hero.js",
+    "./src/components/Content.js",
+    "./src/components/Footer.js",
+    "./src/pages/about.js",
+    "./src/pages/menu.js",
+    "./src/pages/contact.js",
+    "./src/components/Dropdown.js"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
